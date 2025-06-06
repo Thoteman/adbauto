e36b965aa21f2c151bdc4974d2a2a01a832fcd4a
Thomas Knoops <thomas@knoops.be>
2025-06-06 14:46:58 +0200

    fix: new release

--- adbauto/__init__.py
+++ adbauto/__init__.py
@@ -1,9 +1,9 @@
-from .adb import get_ldplayer_device, shell, list_devices
+from .adb import get_emulator_device, shell, list_devices
 from .screen import capture_screenshot, tap_image, tap_img_when_visible
 from .input import tap, scroll
 
 __all__ = [
-    "get_ldplayer_device",
+    "get_emulator_device",
     "shell",
     "list_devices",
     "capture_screenshot",
