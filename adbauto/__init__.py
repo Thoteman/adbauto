6ea4d8d8d1151357a8d1eb1b9d5b0606916aa15d
ThomasKnoops <thomas@knoops.be>
2025-06-09 23:13:47 +0200

    fix: false import

--- adbauto/__init__.py
+++ adbauto/__init__.py
@@ -1,5 +1,5 @@
 from .adb import get_emulator_device, shell, list_devices
-from .screen import capture_screenshot, tap_image, tap_img_when_visible
+from .screen import capture_screenshot, find_image, tap_image, tap_img_when_visible
 from .input import tap, scroll
 
 __all__ = [
